46e2197917cef23c8ed6861feb8e2b7bcaae9b4a
TrieuNT.mbf <nguyenthanhtrieu90@gmail.com>
2021-07-07 14:04:07 +0700

    first commit

--- config.const.php
+++ config.const.php
@@ -2,7 +2,7 @@
 const CRM_URL = "http://localhost/vtigercrm/webservice.php";
 const CRM_USER = "admin";
 const CRM_ACCESS_KEY = "knJmsg6InEZm28PO";
-const APP_DEBUG = false;
+const APP_DEBUG = true;
 
 const APP_ROOT_PATH = 'index.php';
 const NAME_SAPCE_CONTROLLER ="App\Controller\\";
